33f9ce6e4be144bb673b46f0b61e22e7c28b6636
zzleomar <dmy.leomar@gmail.com>
2021-11-04 17:11:35 -0400

    improve configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,9 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
   target: 'static',
+  router: {
+    base: '/<repository-name>/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
